c141f5b5024eeaaa573e5d4b90d7621e986caa90
thebeardless <jakepeterwebster@gmail.com>
2021-04-07 21:52:04 +1000

    Added POST and nifty security permit

--- server/app/controllers/v1/snacks_controller.rb
+++ server/app/controllers/v1/snacks_controller.rb
@@ -1,10 +1,26 @@
 class V1::SnacksController < ApplicationController
 
   def index
-
-    render json: Snack.all # return all snacks
-
+    render json: Snack.all # return all snacks from db.
   end
 
+
+  def create
+    # check model is valid before testing.
+    # snack = Snack.new(name: params[:name], description: params[:description], rating: params[:rating])
+    snack = Snack.new(snack_params)
+    if snack.save
+      render json: snack, status: :created # status 201
+    else
+      render json: snack.errors, status: :unprocessable_entity # status 422
+    end
+  end
+
+  # this can be used to prevent someone passing on fake params to the database.
+  # ie not permitting rating will result in a null rating if used.
+  private 
+  def snack_params
+  params.require(:snack).permit(:name, :description, :rating)
+  end
 end
 
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  get 'snacks/index'
+
   # browser will be /v1/users
+
   namespace :v1 do
-    resources :snacks
+
+    resources :snacks#, only: [:index, :create] #uses only the routes in the array.
+
     resources :users
+
   end
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
+
